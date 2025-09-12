6ef521589c88d542e95cf9859cff4aa23230a33b
givtrah <givtrah@givtrah.org>
2025-09-12 08:09:23 +0200

    font fixes

--- home/features/fonts.nix
+++ home/features/fonts.nix
@@ -1,17 +1,28 @@
 { config, pkgs, ...}: {
 
-  home.packages = with pkgs; 
+	fonts.fontconfig.enable = true;
 
-  [
+	home.packages = with pkgs; [
+  	font-awesome
+		google-fonts
+		corefonts
+		vista-fonts
+		noto-fonts
+  	noto-fonts-cjk-sans
+  	noto-fonts-emoji
+  	liberation_ttf
+  	fira-code
+  	fira-code-symbols
+  	mplus-outline-fonts.githubRelease
+#  	dina-font
+#  	proggyfonts
+		nerd-fonts.jetbrains-mono
+		nerd-fonts.fira-code
+		nerd-fonts.droid-sans-mono
+		nerd-fonts.hack
+		nerd-fonts.iosevka
 
-  font-awesome
-  google-fonts
-  corefonts
-  vistafonts
-#  nerdfonts
-  nerd-fonts.jetbrains-mono
-
-  ];
+	];
 
 
 }
