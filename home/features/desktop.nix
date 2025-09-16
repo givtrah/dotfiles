0d8d0f800e6194d9586c083b091a78d542556603
givtrah <givtrah@givtrah.org>
2025-09-16 09:00:52 +0200

    zotero to beta

--- home/features/desktop.nix
+++ home/features/desktop.nix
@@ -79,7 +79,7 @@
     then [ zotero-nix.packages.aarch64-linux.default]
   else
     (if (pkgs.system == "x86_64-linux")
-      then [ makemkv zotero zoom-us steam-run zettlr nomachine-client calibre 
+      then [ makemkv zotero-beta zoom-us steam-run zettlr nomachine-client calibre 
 
     upscayl  # AI upscaler
         ]
