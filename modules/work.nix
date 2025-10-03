24801655a10cbb56e373a226dd4b778da5c00c41
givtrah <givtrah@givtrah.org>
2025-10-03 08:14:33 +0200

    nfs work

--- modules/work.nix
+++ modules/work.nix
@@ -5,8 +5,24 @@
 
   networking.timeServers = [ "ntp.ku.dk" ]; 
 
+	fileSystems."/mnt/slow" = {
+		device = "100.86.219.83:/slow";
+		fsType = "nfs";
+		options = [ "x-systemd.automount" "noauto" "nfsvers=4.2" ];
+	};
+
+
+	fileSystems."/mnt/fast" = {
+		device = "100.86.219.83:/fast";
+		fsType = "nfs";
+		options = [ "x-systemd.automount" "noauto" "nfsvers=4.2" ];
+	};
+
 
 
 
 }
+
+
+
 
