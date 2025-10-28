d2cdcb686bc5b7ed77912f1ac6dafa14052d82da
givtrah <givtrah@givtrah.org>
2025-10-28 07:44:04 +0100

    disable qemu for taupa, nix flake update

--- hosts/taupa/default.nix
+++ hosts/taupa/default.nix
@@ -126,22 +126,22 @@ nixpkgs.config.permittedInsecurePackages = [
   # List services that you want to enable:
 
   # Virt-manager and Qemu - remember ovmf package for fake secure boot
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-	packages = [(pkgs.OVMF.override {
-	  secureBoot = true;
-	  tpmSupport = true;
-	  }).fd];
-	};
-      };
-    };
-  programs.virt-manager.enable = true;
+#  virtualisation.libvirtd = {
+#    enable = true;
+#    qemu = {
+#      package = pkgs.qemu_kvm;
+#      runAsRoot = true;
+#      swtpm.enable = true;
+#      ovmf = {
+#        enable = true;
+#	packages = [(pkgs.OVMF.override {
+#	  secureBoot = true;
+#	  tpmSupport = true;
+#	  }).fd];
+#	};
+ #     };
+#    };
+#  programs.virt-manager.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
