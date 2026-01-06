fbea1aa9db3344e08beb5a6bc8dc81f53eba763f
givtrah <givtrah@givtrah.org>
2026-01-06 17:59:03 +0100

    new install taupa

--- hosts/taupa/disko-config.nix
+++ hosts/taupa/disko-config.nix
@@ -1,8 +1,9 @@
 {
   disko.devices = {
-    disk.nvme = {
+    disk = {
+      disk1 = {
       type = "disk";
-      device = "/dev/nvme0n1";
+      device = "/dev/nvme1n1";
       content = {
         type = "gpt";
         partitions = {
@@ -23,7 +24,7 @@
               };
             };
           root = {
-            size = "200G";
+            size = "100%";
             content = {
               type = "btrfs";
               extraArgs = [ "-f" ]; # Override existing partition, probably means format it
@@ -43,21 +44,31 @@
                 };
               };
             };
-           luks = {
-             size = "100%";
-             content = {
-               type = "luks";
-               name = "crypted";
-               extraOpenArgs = [ "--allow-discards" ];
-               content = {
-                 type = "btrfs";
-                 extraArgs = [ "-f" ];
-                 
-                 subvolumes = {
-                   "/home" = {
-                     mountpoint = "/home";
-                     mountOptions = [ "compress=lzo" "noatime" ];
-                     };
+           };
+         };
+       };
+    disk2 = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          luks = {
+            size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ "--allow-discards" ];
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+
+                  subvolumes = {
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [ "compress=lzo" "noatime" ];
+                      };
+                    };
                   };
                 };
               };
