7aa3cb6953783073a668fbbdbef9662d14b5ca8f
givtrah <givtrah@givtrah.org>
2026-01-09 09:34:20 +0100

    eh

--- modules/llm.nix
+++ modules/llm.nix
@@ -5,8 +5,20 @@
   environment.systemPackages = with pkgs; [
 
   lmstudio
+	llama-cpp-vulkan
+	ollama-vulkan
 
   ];
+
+	services.ollama = {
+	  package = pkgs.ollama-rocm;
+		enable = true;
+#		acceleration = "rocm";
+#		loadModels = [ "gpt-oss:20b" "gpt-oss:120b" "devstral-small-2:24b" "qwen3-coder:30b" "gemma3:27b" "olmo-3.1:32b" "mistral-3:14b" ];
+		
+		};
+
+  services.open-webui.enable = true;
 
 
 }
