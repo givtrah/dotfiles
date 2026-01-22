98e89380790e134a8d46426ad76cd278832fb81c
givtrah <givtrah@givtrah.org>
2026-01-22 08:15:49 +0100

    remove xfce.pkgs and just keep pkgs

--- home/features/mimetypes.nix
+++ home/features/mimetypes.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ...}: {
 
-home-manager.users.${config.home.username} = {
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
       "application/pdf" = "okularApplication_pdf.desktop";
     };
   };
-};
 
 }
