a569475f81aac643a6665a80c672b2e8de9ff95d
givtrah <givtrah@givtrah.org>
2026-06-03 14:57:23 +0200

    flatpaks

--- home/features/flatpak.nix
+++ home/features/flatpak.nix
@@ -20,10 +20,14 @@
     "com.github.IsmaelMartinez.teams_for_linux"
     "com.github.tchx84.Flatseal"
 		"org.onlyoffice.desktopeditors" # Onlyoffice (should work on both x86_64 and aarch64 platforms)
-		"com.collaboraoffice.Office" # Collabora office (currently plugins not working, should work on x86_64 and aarch64 platforms)
+#		"com.collaboraoffice.Office" # Collabora office (currently plugins not working, should work on x86_64 and aarch64 platforms) - fucking horrible shit show, do not use for now 2026-06-03
 		"org.libreoffice.LibreOffice" # should work on both x86_64 and aarch64
 
 		"com.valvesoftware.Steam"
+
+    "com.heroicgameslauncher.hgl"
+
+    "org.freedesktop.Platform.VulkanLayer.MangoHud"
 
 
 #    "xyz.armcord.ArmCord"
