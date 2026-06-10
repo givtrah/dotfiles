9d9fad393db1a5fa9791aed06b9fd29597d78975
givtrah <givtrah@givtrah.org>
2026-06-10 08:26:07 +0200

    virtbox

---
+++ modules/virt.nix
@@ -0,0 +1,24 @@
+{ pkgs, lib, ... }:
+{
+
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "ohm" ];
+
+
+ # Useful other development tools
+  environment.systemPackages = with pkgs; [
+
+
+
+  ];
+
+# https://discourse.nixos.org/t/changing-thunar-icon-theme/50178/8
+
+
+	# necessary services
+
+
+
+
+
+}
