c8d4a365d3c12ca0f5cb8322da989f8950f21d11
givtrah <givtrah@givtrah.org>
2026-06-29 15:53:19 +0200

    -ble.sh

--- home/bash.nix
+++ home/bash.nix
@@ -4,7 +4,7 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    blesh = true;
+#    blesh = true;
   
     shellAliases = {
     nboot = "sudo nixos-rebuild boot --flake . --impure";
