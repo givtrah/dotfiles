48ae47ef2d8b70e7ea27d30615501c9a52e4d76f
givtrah <givtrah@givtrah.org>
2026-06-29 20:14:26 +0200

    flatpak fix

--- home/flatpak.nix
+++ home/flatpak.nix
@@ -9,8 +9,8 @@
 #    location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
 #  }];
 
-  services.flatpak.update.auto.enable = false;
-  services.flatpak.uninstallUnmanaged = false;
+  services.flatpak.update.auto.enable = true;
+  services.flatpak.uninstallUnmanaged = true;
 
   services.flatpak.packages = [
     #{ appId = "com.brave.Browser"; origin = "flathub"; }
