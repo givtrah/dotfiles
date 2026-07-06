e4c6a5b3617d6f92751ad9cc919120adbba0f6ab
givtrah <givtrah@givtrah.org>
2026-07-06 09:45:39 +0200

    locale

--- modules/locale_tz.nix
+++ modules/locale_tz.nix
@@ -6,17 +6,20 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_DK.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "da_DK.UTF-8";
-    LC_IDENTIFICATION = "da_DK.UTF-8";
-    LC_MEASUREMENT = "da_DK.UTF-8";
-    LC_MONETARY = "da_DK.UTF-8";
-    LC_NAME = "da_DK.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "da_DK.UTF-8";
-    LC_TELEPHONE = "da_DK.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+  i18n.extraLocaleSettings = {                                                                                                
+  LC_ADDRESS = "en_DK.UTF-8";                                                                     
+  LC_IDENTIFICATION = "en_DK.UTF-8";                                                              
+  LC_MEASUREMENT = "en_DK.UTF-8";                                                                 
+  LC_MONETARY = "en_DK.UTF-8";                                                                    
+  LC_NAME = "en_DK.UTF-8";                                                                        
+  LC_NUMERIC = "en_DK.UTF-8";                                                                     
+  LC_PAPER = "en_DK.UTF-8";                                                                       
+  LC_TELEPHONE = "en_DK.UTF-8";                                                                   
+  LC_TIME = "en_DK.UTF-8";                                                                        
+  };     
+
+
+
 
   # Configure console keymap
   console.keyMap = "dk-latin1";
@@ -30,16 +33,18 @@
 
 }
 
-# MAY WANT TO CONSIDER (taking from fresh install 2026-05-18)
 
-#      i18n.extraLocaleSettings = {                                                                                                
-#         LC_ADDRESS = "en_DK.UTF-8";                                                                     
-#         LC_IDENTIFICATION = "en_DK.UTF-8";                                                              
-#         LC_MEASUREMENT = "en_DK.UTF-8";                                                                 
-#         LC_MONETARY = "en_DK.UTF-8";                                                                    
-#         LC_NAME = "en_DK.UTF-8";                                                                        
-#         LC_NUMERIC = "en_DK.UTF-8";                                                                     
-#         LC_PAPER = "en_DK.UTF-8";                                                                       
-#         LC_TELEPHONE = "en_DK.UTF-8";                                                                   
-#         LC_TIME = "en_DK.UTF-8";                                                                        
-#      };            
+#      "OLD" locale settings
+#  i18n.extraLocaleSettings = {
+#    LC_ADDRESS = "da_DK.UTF-8";
+#    LC_IDENTIFICATION = "da_DK.UTF-8";
+#    LC_MEASUREMENT = "da_DK.UTF-8";
+#    LC_MONETARY = "da_DK.UTF-8";
+#    LC_NAME = "da_DK.UTF-8";
+#    LC_NUMERIC = "en_US.UTF-8";
+#    LC_PAPER = "da_DK.UTF-8";
+#    LC_TELEPHONE = "da_DK.UTF-8";
+#    LC_TIME = "en_US.UTF-8";
+#  };
+
+
