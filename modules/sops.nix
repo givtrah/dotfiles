8fcb14c73a861281bbd6e1afe461cc2fda311fde
givtrah <givtrah@givtrah.org>
2026-07-07 22:30:47 +0200

    sops

--- modules/sops.nix
+++ modules/sops.nix
@@ -39,20 +39,20 @@
       };
     };
   };
-  
+
+  # Ensure permissions on keys.txt are fine
   systemd.tmpfiles.rules = [
-    # Allow root and system processes to traverse the sops-nix folder, but keep it clean
+    # Allow root and system processes to traverse the sops-nix folder
     "d /var/lib/sops-nix 0755 root root - -"
     
-    # Make username the absolute owner of the actual key file (this requires username to be in users, which is default)
+    # Make username the sole owner of keys.txt file (this requires username to be in users, which is default)
     "f /var/lib/sops-nix/keys.txt 0600 ${username} users - -"
-    
-    # Create the home configuration directory structure for your user
-    "d /home/${username}/.config/sops/age 0700 ${username} users - -"
-    
-    # 4. Create the declarative symlink pointing back to your user-owned /var/lib file
-    "L+ /home/${username}/.config/sops/age/keys.txt - - - - /var/lib/sops-nix/keys.txt"
   ];
+
+  # point userspace (for all users.. sic, not sure how to fix this) to the key file
+  environment.sessionVariables = {
+    SOPS_AGE_KEY_FILE = "/var/lib/sops-nix/keys.txt";
+  };
 
 
   environment.systemPackages = with pkgs; [ sops ];
