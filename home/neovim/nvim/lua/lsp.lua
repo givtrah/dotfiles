a31b0b36b9d39dd4be5ec7e235b06d76b26202b6
givtrah <givtrah@givtrah.org>
2026-07-07 08:39:43 +0200

    nvim

--- home/neovim/nvim/lua/lsp.lua
+++ home/neovim/nvim/lua/lsp.lua
@@ -22,6 +22,56 @@ vim.lsp.config("yaml_language-server", {})
 vim.lsp.config("tsserver", {})
 vim.lsp.config("r_languageserver", {})
 
+-- get rid of lsp warnings for filetypes
+vim.filetype.add({
+  extension = {
+    mdx = 'markdown.mdx',
+    gotmpl = 'gotmpl',
+  },
+  filename = {
+    ['docker-compose.yml'] = 'yaml.docker-compose',
+    ['docker-compose.yaml'] = 'yaml.docker-compose',
+    ['gitlab-ci.yml'] = 'yaml.gitlab',
+    ['gitlab-ci.yaml'] = 'yaml.gitlab',
+    ['values.yaml'] = 'yaml.helm-values',  -- Added for Helm
+    ['values.yml'] = 'yaml.helm-values',   -- Added for Helm
+  },
+  -- Optional: If you use custom names like my-values.yaml, catch them by pattern
+  pattern = {
+    ['.*values%.yaml'] = 'yaml.helm-values',
+    ['.*values%.yml'] = 'yaml.helm-values',
+  }
+})
+
+-- r languageserver setup
+local r_lsp_config = {
+  cmd = { 'R', '--no-echo', '-e', 'languageserver::run()' },
+  filetypes = { "r" }, 
+  root_dir = function(bufnr, on_dir)
+    -- Fallback to your home directory if a .git folder isn't found
+    on_dir(vim.fs.root(bufnr, '.git') or vim.uv.os_homedir())
+  end,
+  settings = {
+    r = {
+      lsp = {
+        rich_documentation = false,
+      },
+      server_capabilities = {
+        -- Prevents R from trying to format your code (useful if using styler or null-ls)
+        documentFormattingProvider = false,
+        documentRangeFormattingProvider = false,
+      }
+    },
+  },
+}
+vim.lsp.config('r-languageserver', r_lsp_config)
+
+
+
+
+
+
+
 
 
 vim.lsp.enable({
@@ -29,7 +79,7 @@ vim.lsp.enable({
     "lua_ls",
     "marksman",
     "pyright",
-    "r_languageserver",
+    "r-languageserver",
     "rust_analyzer",
     "tinymist",
     "tsserver",
