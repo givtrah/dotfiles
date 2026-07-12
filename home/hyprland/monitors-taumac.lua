9106b4d72148edad7191d7ddfa505ba276c655c8
givtrah <givtrah@givtrah.org>
2026-07-12 22:25:35 +0200

    flake + taumac monitor

--- home/hyprland/monitors-taumac.lua
+++ home/hyprland/monitors-taumac.lua
@@ -1,18 +1,8 @@
 -- ==========================================
--- DISPLAY CONFIGURATION: Laptop Profiles
+-- FALLBACK DEFAULT MONITOR CONFIGURATION
 -- ==========================================
-
--- Fallback dynamic layout discovery
 hl.monitor({ name = "", resolution = "preferred", position = "auto", scale = "auto" })
 
--- 2. Persistent Workspaces & Defaults
 for i = 1, 7 do
-    hl.workspace_rule({ workspace = tostring(i), monitor = "eDP-1", persistent = true })
+  hl.workspace_rule({ workspace = tostring(i), persistent = true })
 end
-for i = 8, 10 do
-    hl.workspace_rule({ workspace = tostring(i), monitor = "DP-2", persistent = true })
-end
-
--- Focus targets
-hl.workspace_rule({ workspace = "1", monitor = "eDP-1", default = true })
-hl.workspace_rule({ workspace = "8", monitor = "DP-2", default = true })
