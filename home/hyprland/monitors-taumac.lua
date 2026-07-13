164a9376f9d4e7db52de6f7c5152fc75056628a4
givtrah <givtrah@givtrah.org>
2026-07-13 20:16:53 +0200

    mon

--- home/hyprland/monitors-taumac.lua
+++ home/hyprland/monitors-taumac.lua
@@ -6,10 +6,10 @@ hl.monitor({
    output = "eDP-1",
    mode = "2560x1600",
    position = "0x0",
-   scale = 1
+   scale = 1.5
  })
 
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1.5 })
 
 for i = 1, 7 do
   hl.workspace_rule({ workspace = tostring(i), persistent = true })
