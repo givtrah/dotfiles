7c37823bfd63d81d5b09b4dd1f5685ada7cda6ac
givtrah <givtrah@givtrah.org>
2026-07-18 18:43:22 +0200

    mon fix

--- home/hyprland/monitors-taumac.lua
+++ home/hyprland/monitors-taumac.lua
@@ -6,7 +6,7 @@ hl.monitor({
    output = "eDP-1",
    mode = "2560x1600",
    position = "0x0",
-   scale = 1.5
+   scale = 1.6
  })
 
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1.5 })
